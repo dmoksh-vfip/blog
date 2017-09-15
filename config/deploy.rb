6e698e689585fd144109b9eef62b84afc46572cc
muktesh-verticalfox <mdeshpande@verticalfox.com>
2017-09-15 12:28:08 +0530

    changes shared_path

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :branch, 'master'
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
 # set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
